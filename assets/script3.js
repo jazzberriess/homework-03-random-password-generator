0e9135f13724e881ad34a964c1d9365c5121e4d0
Jazzberries <anime.flick.chick@gmail.com>
2022-03-07 14:26:25 +1100

    Cleaning up comments in script file.

--- assets/script3.js
+++ assets/script3.js
@@ -31,7 +31,7 @@ function generatePassword() {
     }
 
     //Just to see if it works
-    console.log("clear!");
+    // console.log("clear!");
 
     //ask the user to choose a password length between 1 and 128 characters
 
@@ -48,14 +48,14 @@ function generatePassword() {
 
     upper = window.confirm("Would you like to include uppercase characters? OK for Yes, Cancel for No.");
     //Just to make sure it was working
-    console.log(upper);
+    // console.log(upper);
 
 
     //lowercase prompt
 
     lower = window.confirm("Would you like to include lowercase characters? OK for Yes, Cancel for No.");
     //Just to make sure it was working
-    console.log(lower);
+    // console.log(lower);
 
     //numbers prompt
 
@@ -67,7 +67,7 @@ function generatePassword() {
 
     special = window.confirm("Would you like to include special characters? OK for Yes, Cancel for No.");
     //Just to make sure it was working
-    console.log(special);
+    // console.log(special);
     // console.log(passwordLength);
 
 
@@ -82,7 +82,7 @@ function generatePassword() {
 
 
     //depending on what options the user has selected as true, concat those character arrays into another array.
-    //Big thank you to Italy S for posting a similar solution on stackoverflow. I was stuck for hours on this because I wanted a more concise way of adding the characters: https://stackoverflow.com/questions/69851109/js-password-generator-character-length-not-working
+    //Big thank you to Italy S for posting a similar solution on stackoverflow. I was stuck for hours on this because I wanted a more concise way of creating an array based on the user requirements: https://stackoverflow.com/questions/69851109/js-password-generator-character-length-not-working
 
 
     if (upper === true)
@@ -97,22 +97,21 @@ function generatePassword() {
     if (special === true)
         passwordCharacters = passwordCharacters.concat(specialChars);
 
-
     //Just to make sure it was working
-    console.log(passwordCharacters);
+    // console.log(passwordCharacters);
 
 
     //generate a password based on the password length that the user has chosen and select randomly from the passwordCharacters array
 
-    // ATTEMPT NUMBER INFINITY at the whole making the random generation more secure. Okay. So. This below code (and following function) will generate a string based on the required password length AND criteria, then shuffle that based on the principle of the Fisher-Yates (and Durstenfeld's more recent update to it) algorithm.
+    // Big big thank you to Ryan Shillington on Stack Overflow for providing this solution to a question someone else posted: https://stackoverflow.com/questions/9719570/generate-random-password-string-with-requirements-in-javascript/51540480#51540480
 
-    // Big big thank you to Ryan Shillington on Stack Overflow for providing the below solution to a question someone else posted: https://stackoverflow.com/questions/9719570/generate-random-password-string-with-requirements-in-javascript/51540480#51540480
+    // Pretty sure I wouldn't have been able to do this without them
 
-    // Pretty sure I wouldn't have been able to do this without their solution.
+    // ATTEMPT NUMBER INFINITY at making the random generation more secure. Okay. So. This below code (and following function) will generate a string based on the required password length AND criteria, then shuffle that based on the principle of the Fisher-Yates (and Durstenfeld's more recent update to it) algorithm.
+
+    //all of the below if/else statements are so that any password that is true for all 4 criteria will have at least one character of each category in the password. Otherwise, the password will only have characters from the requested criteria.
 
     var randomPassword = Array(passwordLength);
-
-    //all of this is so that any password that is true for all 4 criteria will have at least one character of each category in the password. Otherwise, the password will only have characters from the requested criteria.
 
     if (upper === true) {
         randomPassword[0] = (upperCaseLetters);
@@ -143,11 +142,10 @@ function generatePassword() {
     randomPassword = randomPassword.fill(passwordCharacters, 4);
 
     //Just so I can see what is happening
-    console.log(passwordCharacters);
+    // console.log(passwordCharacters);
+    // console.log(randomPassword);
 
     //generate the password
-
-    console.log(randomPassword);
 
     return shuffleArray(randomPassword.map(function (createPassword) { return createPassword[Math.floor(Math.random() * createPassword.length)] })).join("");
 
